20a70ed828dbf7f83661810d4c9f87fce83e05ca
Priyagupta108 <priyagupta108@github.com>
2024-11-06 15:17:01 +0530

    test Maven

--- build.sbt
+++
@@ -1,7 +0,0 @@
-name := "java-sapmachine"
-
-version := "0.1"
-
-scalaVersion := "2.13.8"
-
-libraryDependencies += "org.scala-lang" % "scala-library" % "2.13.8"
--- src/main/java/com/example/App.java
+++ src/main/java/com/example/App.java
@@ -1,13 +1,27 @@
 package com.example;
 
-/**
- * Hello world!
- *
- */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Maven JDK version:");
+        System.out.println(System.getProperty("java.version"));
+
+        System.out.println("Subprocess JDK version:");
+        runSubprocess();
+    }
+
+    private static void runSubprocess() {
+        try {
+            Process process = new ProcessBuilder("java", "-version").start();
+            BufferedReader reader = new BufferedReader(new InputStreamReader(process.getErrorStream()));
+            String line;
+            while ((line = reader.readLine()) != null) {
+                System.out.println(line);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 }
